a768582b808f730d0240f58323d1a09d86eee85e
jaimesoad <jaime.ac1106@gmail.com>
2020-11-26 19:24:01 -0500

    test

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
+/* 
+ * Este archivo sirve para poder experimentar con cualquier código que piensen puede ser aplicado
+ * al documento del examen sin hacer cambios en el mismo, manteniendo así la integridad del código
+ * del archivo principal.
+ */
 #include <iostream>
 #include <conio.h>
 using namespace std;
 
 int main(){
-    cout << "Hello world!" << endl;
 
     return 0;
 }
